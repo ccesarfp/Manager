1581f2fc278690705c01af394392262818a98855
Caio Padilha <ccfpadilha@hotmail.com>
2023-05-23 21:02:09 -0300

    Starting project

---
+++ src/main/java/com/aula/pomonooo/PomonoooApplication.java
@@ -0,0 +1,13 @@
+package com.aula.pomonooo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PomonoooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PomonoooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aula/pomonooo/PomonoooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aula.pomonooo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PomonoooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
